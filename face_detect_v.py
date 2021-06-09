498d04ad11332e153be0cbfa7c960a440f1b962e
clifford Ijelekhai <71301809+ohislee@users.noreply.github.com>
2021-06-09 08:56:11 +0100

    added code for saving the video in mp4 format

--- face_detect_v.py
+++ face_detect_v.py
@@ -1,41 +1,44 @@
 import cv2 as cv
 
-def rescaleFrame(frame, scale = 0.6):
-    width = int(frame.shape[1] * scale)
-    height = int(frame.shape[0] * scale)
-
-    dimensions = (width, height)
-    return cv.resize(frame, dimensions, interpolation= cv.INTER_AREA)
-
 capture = cv.VideoCapture('Videos/Minimie.mp4')
 
+fps = capture.get(cv.CAP_PROP_FPS)  #FPS How many frames are transmitted per second
+w = int(capture.get(cv.CAP_PROP_FRAME_WIDTH)) 
+h = int(capture.get(cv.CAP_PROP_FRAME_HEIGHT))
+size = (w, h)
+
+video_output = cv.VideoWriter('Videos/moving faces.mp4', cv.VideoWriter_fourcc(*'mp4v'), fps, size)
+
+
+i = 0  # nFrames: (0 -> nframes-1)
 while True:
     isTrue, frame = capture.read()
 
-    frame_resized = rescaleFrame(frame)
-    gray = cv.cvtColor(frame_resized, cv.COLOR_BGR2GRAY)
-    #canny = cv.Canny(frame_resized,125,175)
-
-    #cv.imshow('Video', frame)
-
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #convert color to gray
+    
     haar_cascade = cv.CascadeClassifier('haar_face.xml')
-
     faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors = 3, minSize = (30,30))
-
     print(f'Number of faces found = {len(faces_rect)}')
 
     for (x,y,w,h) in faces_rect:
-        cv.rectangle(frame_resized, (x,y), (x+w, y+h), (0,255,0), thickness=2)
+        cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), thickness=2)
 
- 
     
-    cv.imshow('Video Resized', frame_resized)
-    #cv.imshow('Gray', gray)
-    #cv.imshow('Detected Faces', canny)
+    
+        #saving output
+    if isTrue:
+        video_output.write(frame)
+    else:
+        break
+    i += 1
+
+    cv.imshow('Moving faces', frame)
 
     if cv.waitKey(20) & 0xFF == ord('d'):
-        break
+        break    
 
 capture.release()
+video_output.release()
 cv.destroyAllWindows()
 
+print('The video was successfully saved.')
